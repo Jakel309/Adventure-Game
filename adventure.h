8c304029e3c3272e9041a75107631c6923e8e585
Justin Foley <justinrfoley@yahoo.com>
2013-10-17 09:18:07 -0500

    Updated header and design

--- adventure.h
+++ adventure.h
@@ -5,21 +5,19 @@ class Level{
 public:
 	Level(int x, int y){}//Initializes with the size of the grid of the map
 	~Level();//Deconstructor
-	void addRooms(Room *room);//Add room to rooms array
-	Room getRoom(int x, int y);//Get room according to x,y-coordinates
+	//void addRooms(Room *room);//Add room to rooms array
+	//Room getRoom(int x, int y);//Get room according to x,y-coordinates
 private:
-	Room **roomsX;//Array of rooms according to x coordinate
-	Room **roomsY;//Array of rooms according to y coordinate
+	//Room **roomsX;//Array of rooms according to x coordinate
+	//Room **roomsY;//Array of rooms according to y coordinate
 };
 
 class Room{
 public:
 	Room(int _xCoord, int _yCoord, string *_commands, string desc);//Initialize room with the x,y-coordinates, a list of commands, and the description of the room
 	~Room();//Deallocates memory
-	void addItem(Item item);//Adds an item to items array
-	void removeItem(Item item);//Removes an item from items array
-	void addDoors(int x, int y);//Adds a door to doors array
-	void removeDoors(int x, int y);//Removes a door from doors array
+	void addItem(Item *item);//Adds an item to items array
+	void removeItem(Item *item);//Removes an item from items array
 	void updateDesc(string desc);//Changes description of room
 	void addCommand(string com);//Adds a command from commands array
 	void removeCommand(string com);//Removes a command from commands array
@@ -28,28 +26,31 @@ public:
 	void commandsAvailable() const;//Prints commands available in room
 	void descriptionOfRoom() const;//Prints description of the room
 	bool ifVisited() const;//Checks to see if room is visited
-	int getXCoord() const;//Gets x-coordinate
-	int getYCoord() const;//Gets y-coordinate
+	Room westRoom() const;//Gets west room
+	Room northRoom() const;//Gets north room
+	Room eastRoom() const;//Gets east room
+	Room southRoom() const;//Gets south room
+	void westRoom(Room *room);//Sets west room
+	void northRoom(Room *room);//Sets north room
+	void southRoom(Room *room);//Sets south room
+	void eastRoom(Room *room);//Sets east room
 private:
-	int xCoord;//Position of room in the level according to x coordinate
-	int yCoord;//Position of room in the level according to y coordinate
 	Item **items;//List of items in room
-	int *doorsX;//List of x-coordinates that point to where the player can go
-	int *doorsY;//List of y-coordinates that point to where the player can go
 	string *commands;//List of commands
 	string description;//Description of room
 	bool visited;//Variable to determine if room has been visited
+	Room **westRoom;
+	Room **northRoom;
+	Room **southRoom;
+	Room **eastRoom;
 };
 
-class Item(){
+class Item{
 public:
 	Item(string _name, bool pickup, string desc);//Initializes item with the name, whether it can be picked up, and it's description
 	~Item();//Deconstructor
 	string getName() const;//Gets name
-	void setName(string name);//Sets name
 	string getDescription() const;//Gets description
-	void setDescription(string desc);//Sets description
-	void InteractWith(Item *toInteract);//Adds item to list of items this item can interact with
 private:
 	string name;//Name of item
 	string description;//Description of item
@@ -57,26 +58,24 @@ private:
 	Item **canInteractWith;//List of items this item can interact with
 }
 
-class People(){
+class People{
 public:
-	People(string _name, int x, int y, string *_commands);//Initializes people with name and x,y-coordinates, and list of commands
+	People(string _name, Room *_room, string *_commands);//Initializes people with name and x,y-coordinates, and list of commands
 	~People();//Deconstructor
-	int getXCoord() const;//Gets x-coordinate
-	int getYCoord() const;//Gets y-coordinate
 	void setPosition(int x, int y);//Set position
 	string getName() const;//Gets name
-	void setName(string name);//Sets name
 	void getCommands() const;//Prints commands available
 private:
+	Room **inRoom;
 	string name;//Name of people
 	int xCoord;//X position
 	int yCoord;//Y position
 	string *commands;//List of commands available
 }
 
-class Player: public People(){
+class Player: public People{
 public:
-	Player(string _name, int x, int y, string *_commands);//Initializes with name of player, x,y-coordinates, and list of commands available to player
+	Player(string _name, Room *_room, string *_commands);//Initializes with name of player, x,y-coordinates, and list of commands available to player
 	~Player();//Deconstructor
 	void addItem(Item *newItem);//Adds item to inventory
 	void removeItem(Item *toRemove);//Removes item from inventory
@@ -84,12 +83,12 @@ private:
 	Item **inventory;//List of items in inventory
 }
 
-class NPC:public People(){
+class NPC:public People{
 public:
-	NPC(string _name, int x, int y, string *_commands, bool _kill);//Initializes with name of enemy, x,y-coordinates, list of commands available, and if it kills when encountered
+	NPC(string _name, Room *_room, string *_commands, bool _kill);//Initializes with name of enemy, x,y-coordinates, list of commands available, and if it kills when encountered
 	~NPC();//Deconstructor
 	bool ifKill() const;//Checks if the NPC kills
-	void changeKill(bool _kill);//Changes if enemy kills
+	void changeKill(bool _kill);//Changes if NPC kills
 private:
 	bool kill;//Variable to see if enemy kills
 }
